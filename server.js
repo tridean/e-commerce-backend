ef288d42086819e1a05e6f76b118932747cea864
tridean <tridean97@gmail.com>
2023-09-04 23:18:11 -0500

    server.js fixes

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
+// Import required modules and dependencies
 const express = require('express');
 const routes = require('./routes');
-const sequelize = require('./models'); // Import your Sequelize instance and models
+const sequelize = require('./config/connection'); // Import your Sequelize instance and models
 
+// Create an Express application
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Middleware to parse JSON and urlencoded request bodies
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Mount your routes on the Express app
 app.use(routes);
 
-// Sync sequelize models to the database, then turn on the server
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}!`);
+// Sync sequelize models to the database, then start the server
+sequelize.sync({ force: false }) // Set force to false to avoid dropping tables on every sync
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`App listening on port ${PORT}!`);
+    });
+  })
+  .catch((err) => {
+    console.error('Error syncing database:', err);
   });
-});
